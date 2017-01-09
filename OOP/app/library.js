f98769a133b29767a7606a63b0c96f4a0e8593fd
Olarewaju Opeyemi <olarewajuakeemopeyemi@gmail.com>
2017-01-09 02:02:07 +0100

    modified library.js to include prototypes

--- OOP/app/library.js
+++ OOP/app/library.js
@@ -1,34 +1,40 @@
 'use strict'
 
-module.exports = {
 
-  Car: function(name, model, type) {
-    this.name = name || 'General';
-    this.model = model || 'GM';
-    this.type = type;
-    this.speed = '0 km/h';
-    if (this.name === 'Porshe' || this.name === 'Koenigsegg') {
-      this.numOfDoors = 2;
-    } else {
-      this.numOfDoors = 4;
-    };
-    if (this.type === 'trailer') {
-      this.numOfWheels = 8;
-      this.isSaloon = false;
-    } else {
-      this.numOfWheels = 4;
-      this.isSaloon = true;
-    };
-    this.drive = function(num) {
-
-      if (this.type === 'trailer') {
-        var temp = 11 * num;
-        this.speed = temp + ' km/h';
-      } else {
-        var temp = 50 * num;
-        this.speed = temp + ' km/h';
-      };
-      return this;
-    };
-  }
+function Car(name, model, type) {
+  this.name = name || 'General';
+  this.model = model || 'GM';
+  this.type = type;
+  this.speed = '0 km/h';
+  if (this.name === 'Porshe' || this.name === 'Koenigsegg') {
+    this.numOfDoors = 2;
+  } else {
+    this.numOfDoors = 4;
+  };
+  if (this.type === 'trailer') {
+    this.numOfWheels = 8;
+    this.isSaloon = false;
+  } else {
+    this.numOfWheels = 4;
+    this.isSaloon = true;
+  };
 }
+Car.prototype.drive = function(num) {
+
+    if (this.type === 'trailer') {
+      var temp = 11 * num;
+      this.speed = temp + ' km/h';
+    } else {
+      var temp = 50 * num;
+      this.speed = temp + ' km/h';
+    };
+    return this;
+  }
+Car.prototype.carspeed = function() {
+    console.log( "I'm rolling at " + this.speed + " speed" );
+}
+
+Car.prototype.displayname = function() {
+    console.log( this.name + " is my car name!" );
+}
+module.exports.Car = Car;
