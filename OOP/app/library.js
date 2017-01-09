24fd5ddf4c6846f1d2b1e0127946aa96442f3514
Olarewaju Opeyemi <olarewajuakeemopeyemi@gmail.com>
2017-01-09 07:54:40 +0100

    added inheritance features to the class

--- OOP/app/library.js
+++ OOP/app/library.js
@@ -21,20 +21,33 @@ function Car(name, model, type) {
 }
 Car.prototype.drive = function(num) {
 
-    if (this.type === 'trailer') {
-      var temp = 11 * num;
-      this.speed = temp + ' km/h';
-    } else {
-      var temp = 50 * num;
-      this.speed = temp + ' km/h';
-    };
-    return this;
-  }
+  if (this.type === 'trailer') {
+    var temp = 11 * num;
+    this.speed = temp + ' km/h';
+  } else {
+    var temp = 50 * num;
+    this.speed = temp + ' km/h';
+  };
+  return this;
+}
 Car.prototype.carspeed = function() {
-    console.log( "I'm rolling at " + this.speed + " speed" );
+  console.log("I'm rolling at " + this.speed + " speed");
 }
 
 Car.prototype.displayname = function() {
-    console.log( this.name + " is my car name!" );
+  console.log(this.name + " is my car name!");
 }
+
+function FutureCar(name, model, type, flyHeight) {
+  // Call the parent constructor. Note we can now pass arguments too. Woop!
+  Car.call(this, name, model, type);
+
+  this.flyHeight = flyHeight;
+}
+// And we inherit the parent prototype
+FutureCar.prototype = Object.create(Car.prototype);
+FutureCar.prototype.fly = function() {
+  console.log("Nice " + this.name + " " + this.model + ", can fly to a height of " + this.flyHeight);
+};
+
 module.exports.Car = Car;
